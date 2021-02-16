c4701d73be5f0ec40041a98949a8abb1660bceaa
Beatriz Fernandes <beatrizdl.fernandes@gmail.com>
2021-02-16 15:22:51 -0300

    CRUD done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # mostrar todas as tarefas na tela 
+  get 'tasks', to: 'tasks#index'
+
+  # mostrar um form para o user adicionar nova tarefa
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  # mostrar os detalhes de cada tarefa em uma nova página
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # criar uma nova tarefa pelo form criado
+  post 'tasks', to: 'tasks#create'
+
+  # mostrar um form para user editar uma tarefa já criada
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  # atualizar a task no db
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # deletar uma task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
+
 end
